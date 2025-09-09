18f39c44a2282bafaf85182769e485535394624e
Dmitriy <73019435+dimcheify-prog@users.noreply.github.com>
2025-09-09 11:32:17 +0300

    initial

---
+++ src/components/Block/Block.tsx
@@ -0,0 +1,5 @@
+import * as css from './Block.module.scss';
+
+export const Block = () => {
+  return <div className={css.block}>check this out</div>;
+};
---
+++ src/components/Button.tsx
@@ -0,0 +1,5 @@
+import * as css from "./button.module.scss";
+
+export const Button = () => {
+  return <button className={css.btn}>clifdsafdsfsdck me</button>;
+};
